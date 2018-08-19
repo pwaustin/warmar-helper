a96fef03bd11f43dc8ce265c22060716b0697097
Patrick Austin <42253888+pwaustin@users.noreply.github.com>
2018-08-19 16:15:35 -0700

    Add EV calculation support

--- main.py
+++ main.py
@@ -3,9 +3,4 @@ import marketquery
 import stats
 
 relics = reliclib.generate_relics('relics.csv')
-
-
 relic_price_list = marketquery.get_relic_item_prices('Neo R1', relics)
-
-for item in testrelic:
-    print(item)
