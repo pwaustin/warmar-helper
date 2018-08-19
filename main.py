e2aa8352c610f4791bfd52d7eca0f26ab9872cdb
Patrick Austin <42253888+pwaustin@users.noreply.github.com>
2018-08-19 16:29:09 -0700

    Fix relic drop table values in EV calc

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import stats
 
 relics = reliclib.generate_relics('relics.csv')
 relic_price_list = marketquery.get_relic_item_prices('Neo R1', relics)
+
+print(relic_price_list)
+print(stats.get_expected_value(relic_price_list, 'intact', True, False))
+print(stats.get_expected_value(relic_price_list, 'intact', False, False))
+print(stats.get_expected_value(relic_price_list, 'radiant', True, False))
+print(stats.get_expected_value(relic_price_list, 'radiant', False, False))
--- stats.py
+++ stats.py
@@ -1,22 +1,23 @@
 """
 given a list of prices, return the expected value of the relic based on its quality. average argument controls whether
-average price or lowest price is used in the calculation
+average price or lowest price is used in the calculation. radshare argument supposes EV with 4 identical relics used in
+a run
 """
 
 
 def get_expected_value(prices, relic_quality, take_average, rad_share):
     chance = {'intact': [.2533, .11, .02], 'exceptional': [.2333, .13, .04],
-              'flawless': [.20, .34, .06], 'radiant': [.5, .40, .1]}
-    drop_type = {'bronze': 0, 'silver': 1, 'gold': 2}
+              'flawless': [.20, .17, .06], 'radiant': [.1667, .20, .1]}
+    drop_type = {'common': 0, 'uncommon': 1, 'rare': 2}
     total_expected_value = 0
     if not rad_share:
         for i in range(0, len(prices)):
             if i < 3:
-                item_quality = drop_type['bronze']
+                item_quality = drop_type['common']
             elif i < 5:
-                item_quality = drop_type['silver']
+                item_quality = drop_type['uncommon']
             else:
-                item_quality = drop_type['gold']
+                item_quality = drop_type['rare']
 
             if not take_average:
                 total_expected_value += prices[i][0] * chance[relic_quality][item_quality]
