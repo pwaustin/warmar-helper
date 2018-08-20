9d6a7c0e55449597e5e23ebd22a9b2a51d8bdac1
Patrick Austin <42253888+pwaustin@users.noreply.github.com>
2018-08-20 12:52:05 -0700

    Improve user interface

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import reliclib
 import marketquery
 import stats
 
+use_average = False
 relics = reliclib.generate_relics('relics.csv')
 
 while True:
-    chosen_relic = input('Enter the name of your relic: ')
+    chosen_relic = input('Enter the name of your relic, or select \'exit\': ')
     user_relic = reliclib.make_relic(chosen_relic, relics)
     if chosen_relic == 'exit':
         break
@@ -14,7 +15,7 @@ while True:
         if quality == 'intact' or quality == 'radiant' or quality == 'exceptional' or quality == 'flawless':
             print('Relic has been found!\nProcessing...')
             relic_price_list = marketquery.get_relic_item_prices(chosen_relic, relics)
-            for i in range (0, 6):
+            for i in range(0, 6):
                 if i < 3:
                     print(user_relic.commons[i])
                 elif i < 5:
@@ -22,13 +23,10 @@ while True:
                 else:
                     print(user_relic.rare)
                 print(relic_price_list[i])
-            prices = stats.get_expected_prices(relic_price_list, True)
+            prices = stats.get_expected_prices(relic_price_list, use_average)
             print('Expected value for single use: ' + str(stats.get_expected_value(prices, quality, False)))
             print('Expected value for relic share: ' + str(stats.get_expected_value(prices, quality, True)))
         else:
             print('Error: Invalid relic quality. Please try again.')
     else:
         print('Error: Invalid relic name.')
-
-
-
